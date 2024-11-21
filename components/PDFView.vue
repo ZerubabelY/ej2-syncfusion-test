19f88a601c1862f6d891716d811f0bc200d2e09b
ZerubabelY <zerubabel.yigzaw23@gmail.com>
2024-11-21 14:38:23 +0300

    fix: pdf viewer local pdf loader

--- components/PDFView.vue
+++ components/PDFView.vue
@@ -7,18 +7,31 @@
         PDF Preview
       </h1>
     </div>
-    <div class="w-full h-full" v-if="selectedFile">
-      <ejs-pdfviewer
-        id="pdfviewer"
-        ref="pdfviewer"
-        :documentPath="documentPath"
-        :resourceUrl="resourceUrl"
-      >
-      </ejs-pdfviewer>
+    <div v-if="loading" class="flex items-center justify-center w-full h-full">
+      <div class="flex items-center justify-center h-full">
+        <div
+          class="w-32 h-32 ease-linear border-8 border-t-8 border-gray-200 rounded-full loader"
+        ></div>
+      </div>
     </div>
-    <div v-else class="flex flex-col items-center justify-center h-full gap-5">
-      <img src="/icons/test.png" alt="" class="w-64 h-auto" />
-      <p class="text-lg">No file is selected</p>
+
+    <div v-else class="w-full h-full">
+      <div class="w-full h-full" v-if="selectedFile">
+        <ejs-pdfviewer
+          id="pdfviewer"
+          ref="pdfviewer"
+          :documentPath="documentPath"
+          :resourceUrl="resourceUrl"
+        >
+        </ejs-pdfviewer>
+      </div>
+      <div
+        v-else
+        class="flex flex-col items-center justify-center h-full gap-5"
+      >
+        <img src="/icons/test.png" alt="" class="w-64 h-auto" />
+        <p class="text-lg">No file is selected</p>
+      </div>
     </div>
   </div>
 </template>
@@ -51,9 +64,10 @@ export default {
   },
   data: function () {
     return {
-      documentPath: "https://cdn.syncfusion.com/content/pdf/pdf-succinctly.pdf",
+      documentPath: "",
       resourceUrl:
         "https://cdn.syncfusion.com/ej2/23.2.6/dist/ej2-pdfviewer-lib",
+      loading: false,
     };
   },
   provide: {
@@ -73,6 +87,28 @@ export default {
       PageOrganizer,
     ],
   },
+  async mounted() {
+    if (this.selectedFile?.path && this.$refs.pdfviewer) {
+      this.loading = true;
+      this.documentPath = this.selectedFile.path;
+      try {
+        const res = await fetch(this.documentPath);
+
+        if (res.ok) {
+          const blob = await res.blob();
+          // Change to base64
+          const url = URL.createObjectURL(blob);
+          this.documentPath = url;
+        }
+
+        this.$refs.pdfviewer.ej2Instances.load("", null);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        this.loading = false;
+      }
+    }
+  },
   methods: {
     change: function (args) {
       var viewer = this.$refs.pdfviewer.ej2Instances;
@@ -84,6 +120,37 @@ export default {
       }
       viewer.dataBind();
       viewer.load(viewer.documentPath, null);
+    },
+  },
+
+  watch: {
+    selectedFile: {
+      handler: async function (newVal) {
+        if (!newVal) {
+          this.documentPath = "";
+          return;
+        }
+
+        this.documentPath = newVal.path;
+        this.loading = true;
+        try {
+          const res = await fetch(this.documentPath);
+
+          if (res.ok) {
+            const blob = await res.blob();
+            // Change to base64
+            const url = URL.createObjectURL(blob);
+            this.documentPath = url;
+          }
+
+          this.$refs.pdfviewer.ej2Instances.load("", null);
+        } catch (error) {
+          console.log(error);
+        } finally {
+          this.loading = false;
+        }
+      },
+      immediate: true,
     },
   },
 };
